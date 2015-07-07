6ced5368537ccf13caab8399b0136a9bab6a64e9
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-07 16:10:05 -0500

    initial commit

---
+++ global.js
@@ -0,0 +1,20 @@
+//setting variable using a propt of the user//
+var userName = prompt("What is your name?", "Type your name here");
+var userAge = prompt("How old are you?", "Type your age here");
+var userTown = prompt("What is your hometown?", "Type your hometown here");
+var userNumber = prompt("What is your favorite number?", "Type your favorite number here");
+
+//iterating the user data back in prompts and via console
+alert("your name is" + userName);
+console.log ("user's name is " + userName);
+
+alert("your age is" + userAge);
+console.log ("user's age is " + userAge);
+
+alert("your hometown is" + userTown);
+console.log ("user's hometown is " + userTown);
+
+alert("your favorite number is" + userNumber);
+console.log ("user's favorite number is " + userNumber);
+
+
