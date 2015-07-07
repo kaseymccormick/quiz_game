db5bc81c69facaec3a93d3e24599eb4a5bdff337
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-07 17:29:24 -0500

    file setup changes window.onload

--- v2/global.js
+++ v2/global.js
@@ -1,3 +1,9 @@
+//to get css to load first doing window.onload
+window.onload = function(){
+  // All the rest of your Javascript code
+
+
 var questions = ["Which is not true about Dr. Suess's?", "What phrase is from Green Eggs and Ham? ", "Which Dr. Suess story does not have a movie?", "Which is not a Dr. Suess book?"];
 
 
+}
