cadc0476a8e40f186f9bf57a5f9a847280f39f14
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-07 23:05:22 -0500

    up to step 3 part 1
    not able to complete changing button to on_click also need to modify function to fit variables i used.
    Sleep is goal, productivity is lower then simple errors made now.

--- v2/global.js
+++ v2/global.js
@@ -29,18 +29,74 @@ function given_answer(){
 }
 
 var answer_text = given_answer().toLowerCase();
- 
-console.log(answer_text)
 
 
-
+// i'd like to  make this a false so can use falsey logic later but it's a tangled web at the moment//
 function is_correct_answer(answer_text){
-  if (answer_text !== "d")
+  if (answer_text === "d")
  
-  return false 
+  return true 
+    // i want to do return var correct=true but it breaks the code above
   
 }
 
+var correct = "true" // trying to see if can set variable to true force the thing to work
+
+function update_question_result(correct){
+  if ( correct === "true"){ //unexpected identifier likely correct
+  var result = document.getElementById("question_result");
+  result.innerText = ("Correct!");
+
+}else{
+
+  var result = document.getElementById("question_result");
+  result.innerText = ("Sorry, guess different next time.");
+}
+}
+
+function process_answer_submission(){
+  var user_answer = given_answer();
+  update_question_result(is_correct_answer(user_answer));
+}
+
+var optFour = document.getElementById("fourth");
+optFour.innerText = ("D. Has two children");
+<button id="submitter" onclick="process_answer_submission()">Try it</button>
+    <button id="submitter">
+    </button>
+
+Now we want that function to run whenever button#submitter is clicked. You might need to do some research to figure this out. Hint: What you're trying to do here is attach an "event handler" to the button. The "event" is "click". So you're trying to define how the button handles being clicked. Specifically, you're trying to say that the behavior of the button, when it's clicked, should be as described in the process_answer_submission function.
+
+tried below
+given_answer()
+"D"
+var answer_text = given_answer().toLowerCase();
+undefined
+answer_text
+"d"
+is_correct_answer(answer_text)
+true
+correct
+undefined
+correct = "true"
+"true"
+update_question_result(correct)
+undefined
+document.getElementById("submitter")
+<button id=​"submitter">​
+    ​</button>​
+bttn = document.getElementById("submitter")
+<button id=​"submitter">​
+    ​</button>​
+bttn.innerHTML = <button id="submitter" onclick="process_answer_submission()">Try it</button>
+VM884:2 Uncaught SyntaxError: Unexpected token <
+    at Object.InjectedScript._evaluateOn (<anonymous>:895:140)
+    at Object.InjectedScript._evaluateAndWrap (<anonymous>:828:34)
+    at Object.InjectedScript.evaluate (<anonymous>:694:21)InjectedScript._evaluateOn @ VM486:895InjectedScript._evaluateAndWrap @ VM486:828InjectedScript.evaluate @ VM486:694
+bttn.innerHTML = onclick="process_answer_submission()"
+"process_answer_submission()"
+
+errored. out of capacity getting sleep.
 
 ///////EVERYTHING below KILLS the above///////////////////////////////
 
@@ -82,8 +138,5 @@ function is_correct_answer(answer_text){
 // //   var response = ("not the correct answer");
 // // }
 // //
-// // }
-    
-
-
-     
+// // }//
+         
